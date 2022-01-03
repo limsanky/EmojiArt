9b7e973cfcb785038a6256eaaf4aae1d30fe99b3
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-01-03 16:48:43 +0900

    Initial commit

---
+++ EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Sankarshana V on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EmojiArt
+ This is the EmojiArt app, which is made in Swift, developed while watching Stanford's cs193p class.
